f84d149722cc0dc9b3b7e8adea7b2767085b7d7b
cooperkinnan <cooperkinnan@unomaha.edu>
2025-02-06 03:52:24 +0000

    done with lab

--- ApproxPi.py
+++ ApproxPi.py
@@ -12,25 +12,27 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
-precision = int(input("enter precision(1 to 10): "))
-precision = int(precision)
-start = time.time()
+  precision = int(input("enter precision(1 to 10): "))
+  precision = int(precision)
+  start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
-approxpi = 4/1
-sign = -1
-denom = 3
+  approxpi = 4/1
+  sign = -1
+  denom = 3
 
-while round(approxpi , precision) != round(realPi, precision):
-  print(approxpi)
-approxpi = approxpi + (sign * 4 / denom)
+  while round(approxpi , precision) != round(realPi, precision):
+    print(approxpi)
+    approxpi = approxpi + (sign * 4 / denom)
 
-sign = sign * -1
-denom = denom + 2
-end = time.time()
+    sign = sign * -1
+    denom = denom + 2
+  
+  end = time.time()
 
-elapsedTime = end - start
-print(elapsedTime)
+
+  elapsedTime = end - start
+  print(elapsedTime)
 
 if __name__ == '__main__':
   main()
